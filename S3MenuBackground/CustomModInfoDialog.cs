6f07fa7d77959692fe54c37480a7563f1b9b20b3
Arek <Arek@DESKTOP-L2FR9S7>
2025-01-21 17:41:38 +0100

    Only thing left to do is glow rotate windows

---
+++ S3MenuBackground/CustomModInfoDialog.cs
@@ -0,0 +1,62 @@
+﻿using Sims3.UI;
+
+namespace S3MenuBackground
+{
+	public class ModInfoDialogCustom : ModalDialog
+	{
+		public static ModInfoDialogCustom.Result Show(string[] modFileNames)
+		{
+			ModInfoDialogCustom.Result result = ModInfoDialogCustom.Result.OK;
+			if (modFileNames != null && modFileNames.Length > 0)
+			{
+				using (ModInfoDialogCustom modInfoDialog = new ModInfoDialogCustom(modFileNames))
+				{
+					modInfoDialog.StartModal();
+					result = modInfoDialog.mResult;
+					modInfoDialog.Dispose();
+				}
+			}
+			return result;
+		}
+        
+		public ModInfoDialogCustom(string[] packageFileNames) : base("ModInfoDialogCustom", 161369792, true, ModalDialog.PauseMode.PauseTask, null)
+		{
+			this.mOkButton = (this.mModalDialogWindow.GetChildByID(161369840U, true) as Button);
+			this.mTextEdit = (this.mModalDialogWindow.GetChildByID(161369824U, true) as TextEdit);
+			this.mOkButton.Click += this.OnOkClick;
+			string text = string.Empty;
+			foreach (string obj in packageFileNames)
+			{
+				text = text + obj + '\n';
+			}
+			this.mTextEdit.Caption = (string.IsNullOrEmpty(text) ? string.Empty : text);
+		}
+        
+		public void OnOkClick(WindowBase sender, UIButtonClickEventArgs eventArgs)
+		{
+			this.mResult = ModInfoDialogCustom.Result.OK;
+			this.StopModal();
+		}
+        
+		public const string kLayoutName = "ModInfoDialogCustom";
+        
+		public const int kWinExportID = 161369792;
+        
+		public ModInfoDialogCustom.Result mResult;
+        
+		public Button mOkButton;
+
+		public TextEdit mTextEdit;
+        
+		public enum ControlIDs : uint
+		{
+			OkButton = 161369840U,
+			TextEdit = 161369824U
+		}
+        
+		public enum Result
+		{
+			OK
+		}
+	}
+}
--- S3MenuBackground/MyClass.cs
+++ S3MenuBackground/MyClass.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using Sims3.Gameplay;
 using Sims3.Gameplay.Core;
 using Sims3.SimIFace;
 using Sims3.UI;
 using Sims3.UI.GameEntry;
+using Function = Sims3.Gameplay.Function;
 using OneShotFunctionTask = Sims3.Gameplay.OneShotFunctionTask;
 
 namespace S3MenuBackground
@@ -16,39 +18,56 @@ namespace S3MenuBackground
 #pragma warning restore CS0169 // Field is never used
         public static string timeOfDay;
         public static char randomLetter;
-        
+        [Tunable]
+        // ReSharper disable once UnassignedField.Global
+        public static bool bRespectTimeOfDay;
+        public static bool bShownDialog;
+        [Tunable]
+        // ReSharper disable once UnassignedField.Global
+        public static bool bShowMods;
         static Main()
         {
             World.sOnStartupAppEventHandler += OnStartupApp;
             World.sOnEnterNotInWorldEventHandler += OnEnterNotInWorldEventHandler;
-            World.sOnWorldQuitEventHandler += OnWorldQuit;
+            World.sOnLeaveNotInWorldEventHandler += OnLeaveNotInWorld;
         }
 
         private static void OnStartupApp(object sender, EventArgs e)
         {
-            DateTime currentTime = DateTime.Now;
-            timeOfDay = GetDayOrNight(currentTime);
-            Randomize();
             Commands.sGameCommands.Register("cb", "Change Background.", Commands.CommandType.General, (Main.Cheat));
+        }
+
+        public static void GetRandom()
+        {
+            DateTime currentTime = DateTime.Now;
+            if (bRespectTimeOfDay)
+            {
+                timeOfDay = GetDayOrNight(currentTime);
+            }
+            if (!bRespectTimeOfDay)
+            {
+                timeOfDay = GetZeroOrOne().ToString();
+            }
+            RandomizeLetter();
         }
         private static void OnEnterNotInWorldEventHandler(object sender, EventArgs e)
         {
-            Simulator.AddObject(new OneShotFunctionTask(Main.Run, StopWatch.TickStyles.Milliseconds, 1f));
+            GetRandom();
+            Simulator.AddObject(new OneShotFunctionTask(Main.Run, StopWatch.TickStyles.Seconds, 0.1f));
         }
-        private static void OnWorldQuit(object sender, EventArgs e)
+        private static void OnLeaveNotInWorld(object sender, EventArgs e)
         {
-            if (!GameStates.IsTravelling) // if not traveling clear 
+            Simulator.AddObject(new OneShotFunctionTask(() =>
             {
-                DateTime currentTime = DateTime.Now;
-                timeOfDay = GetDayOrNight(currentTime);
-                Randomize();
+                GetRandom();
                 Run();
-            }
+                
+            }, StopWatch.TickStyles.Seconds, 0.1f));
         }
         
         public static int Cheat(object[] parameters)
         {
-            Randomize();
+            GetRandom();
             Run();
             return 1;
         }
@@ -64,7 +83,6 @@ namespace S3MenuBackground
                 return "0";
             }
         }
-
         private static void Run()
         {
             if (GameStates.IsInMainMenuState)
@@ -86,20 +104,60 @@ namespace S3MenuBackground
                         goto Block_9; // Fallback label
                     }
                 }
-
-                // If successful, mainMenu now holds the MainMenu object
+                
                 Block_9:;
                 ImageDrawable background = mainMenu.Drawable as ImageDrawable;
-                background.Image = UIManager.LoadUIImage(ResourceKey.CreatePNGKey(randomLetter + timeOfDay, 0U));
+                string imagename;
+                imagename = randomLetter + timeOfDay;
+                background.Image = UIManager.LoadUIImage(ResourceKey.CreatePNGKey(imagename, 0U));
+                
                 mainMenu.Invalidate();
+                if (!bShownDialog && bShowMods)
+                {
+                    Simulator.AddObject(new OneShotFunctionTask(Main.ModCheck, StopWatch.TickStyles.Seconds, 0.1f));
+                }
             }
         }
 
-        public static void Randomize()
+        private static char previousLetter = '\0'; // Initialize with a value that won't match any valid letter.
+        public static void RandomizeLetter()
         {
             Random random = new Random();
-            int randomIndex = random.Next(0, 3);
-            randomLetter = (char)('a' + randomIndex);
+            char newLetter;
+
+            do
+            {
+                int randomIndex = random.Next(0, 3);
+                newLetter = (char)('a' + randomIndex);
+            } while (newLetter == previousLetter);
+
+            randomLetter = newLetter;
+            previousLetter = randomLetter; // Update the previous letter
+        }
+
+        static int GetZeroOrOne()
+        {
+            Random random = new Random();
+            return random.Next(0, 2);
+        }
+
+        public static void ModCheck()
+        {
+            bShownDialog = true;
+            List<string> list = new List<string>();
+            uint modFilesCount = GameUtils.GetModFilesCount();
+            for (uint num = 0U; num < modFilesCount; num += 1U)
+            {
+                string modFilesName = GameUtils.GetModFilesName(num);
+                if (!string.IsNullOrEmpty(modFilesName))
+                {
+                    list.Add(modFilesName);
+                }
+            }
+            if (list.Count > 0)
+            {
+                ModInfoDialogCustom.Show(list.ToArray());
+            }
         }
     }
 }
